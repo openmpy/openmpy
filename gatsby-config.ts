8e079736e1384da256884922e781e13c19bdb459
openmpy <suhwan@kakao.com>
2024-06-25 09:42:42 +0900

    Initial Gatsby Commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,28 +1,40 @@
-import type { GatsbyConfig } from "gatsby";
+import dotenv from 'dotenv'
+import type { GatsbyConfig } from 'gatsby'
+
+dotenv.config()
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `openmpy blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "XWtPPQ3YghL5WoYsMNfQuY5IxEkd26NKt9SmhYLDD0A",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  jsxRuntime: 'automatic',
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
     },
-    __key: "images"
-  }]
-};
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/',
+      },
+      __key: 'images',
+    },
+  ],
+}
 
-export default config;
+export default config
